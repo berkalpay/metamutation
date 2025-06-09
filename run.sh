3eb52ab0b3d60272c88438fcaa5f97fb5494b7e1
berkalpay <46028827+berkalpay@users.noreply.github.com>
2025-06-09 11:26:33 -0400

    tidy run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,16 @@
-# Make the figures directory if it doesn't exist
-mkdir -p figures
-
-# If the combined data doesn't exist, download and prepare it
-if [ ! -f data/data.csv ]; then
-    cd data
-    ./download.sh
-    Rscript substitutions.R
-    python structural_features.py
-    Rscript data.R
-    cd ..
-fi
+# Download and prepare the data
+cd data
+./download.sh
+Rscript substitutions.R
+python structural_features.py
+Rscript data.R
+cd ..
 
 # Fit the models to the data
 Rscript fit.R
 
 # Run all analyses
+mkdir -p figures
 Rscript analysis/dms_stats.R
 Rscript analysis/aa.R
 Rscript analysis/aa_physicochemical.R
